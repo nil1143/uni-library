73fda03ad914ab5642367d37fb9c5621c48efe60
John Doe <johndoe@email.com>
2025-01-24 21:54:03 +0000

    Header; root folder to crate layout for pages within route group

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+
+const Home = () => (
+  <>
+    <BookOverview />
+    <BookList />
+  </>
+);
+export default Home;
