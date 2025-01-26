9d9dbfbb424bf627ca73c06cec12f6b601cfc76b
John Doe <johndoe@email.com>
2025-01-26 00:05:04 +0000

    Homepage with dummy data; configuration for images url in next.config.ts; SVG component to pass cover color variable

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/contants";
 
 const Home = () => (
   <>
-    <BookOverview />
-    <BookList />
+    <BookOverview {...sampleBooks[1]} />
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 export default Home;
