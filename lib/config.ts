76f3d81a033c1ad1c791220f251cde943e5176f9
John Doe <johndoe@email.com>
2025-01-29 16:44:30 +0000

    typos fix

--- lib/config.ts
+++ lib/config.ts
@@ -9,8 +9,8 @@ const config = {
     },
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
-      redisUrl: process.env.REDIS_URL!,
-      redisToken: process.env.REDIS_TOKEN!,
+      redisUrl: process.env.UPSTASH_REDIS_URL!,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN!,
       qstashUrl: process.env.QSTASH_URL!,
       qstashToken: process.env.QSTASH_TOKEN!,
     },
